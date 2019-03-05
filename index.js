c48a6ab71ee1040efeda58ad87721b5f61701528
cuteapple <hidden>
2019-03-05 10:38:27 +0800

    2box

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 let { Engine, Render, World, Bodies } = Matter
+
+let engine = Engine.create()
+let render = Render.create({
+    element: document.body,
+    engine: engine
+})
+
+///
+/// create items
+///
+
+let boxA = Bodies.rectangle(400, 200, 80, 80)
+let boxB = Bodies.rectangle(450, 50, 80, 80)
+let ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true })
+
+///
+/// start
+///
+
+World.add(engine.world, [boxA, boxB, ground])
+Engine.run(engine)
+Render.run(render)
